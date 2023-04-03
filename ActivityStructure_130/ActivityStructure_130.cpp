0e4a68abeecaeada48b6384e273ba9e933fea94b
Kevinzain29 <ahmad.kevin.ft22@mail.umy.ac.id>
2023-04-03 10:23:53 +0700

    inputan atribut mhs2

--- ActivityStructure_130/ActivityStructure_130.cpp
+++ ActivityStructure_130/ActivityStructure_130.cpp
@@ -10,4 +10,22 @@ struct mahasiswa
     string umur;
 };
 
+int main() {
+    mahasiswa mhs1; mhs2;
+
+    mhs1.NIM = "20220140130";
+    mhs1.nama = "Ahmad Kevin Zain";
+    mhs1.alamat = "Wonosobo";
+    mhs1.umur = "20";
+
+    cout << "Masukan NNIM :";
+    cin >> mhs2.NIM;
+    cout << "Masukan Nama :";
+    cin >> mhs2.Nama;
+    cout << "Masukan Alamat :";
+    cin >> mhs2.alamat;
+    cout << "Masukan Umur :";
+    cin >> mhs2.umur;
+
+}
 
